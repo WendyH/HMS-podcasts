4a714a22a7c0e7c31621289502d73738b21950f2
WendyH <Wendy.Hane@gmail.com>
2018-07-01 16:34:17 +0600

    change decryption

--- HDKinoteatr/MediaResourceLink.cpp
+++ HDKinoteatr/MediaResourceLink.cpp
@@ -1,4 +1,4 @@
-﻿// 2018.04.04
+﻿// 2018.07.01
 ////////////////////////  Получение ссылки на поток ///////////////////////////
 #define mpiJsonInfo 40032
 #define mpiKPID     40033
@@ -181,10 +181,11 @@ void GetLink_Moonwalk(string sLink) {
       HmsLogMessage(2, mpTitle+": Не найдены параметры для POST запроса в функции getVideoManifests."); 
       return; 
     }
-    string iv   = "c46b534f9def34b0f2040a503d978eed";
+    string iv   = "e080ee12a6b39ad18309bc89d5097b77";
     string sKey = "617adae21a8aedc4e13938619b62f4ecdd3b947cd64620569df257d333e4f11d";
     HmsRegExMatch('\\be=[\'"](.*?)[\'"]', sData, sKey);
-    HmsRegExMatch('\\bn=[\'"](.*?)[\'"]', sData, iv  );
+    //HmsRegExMatch('\\bn=[\'"](.*?)[\'"]', sData, iv  );
+    sKey = "7316d0c4"+Trim(sKey); // snx 2 spell!
     POSTDATA.LoadFromString(sJSONParams);
     // Формируем данные для POST
     sPost = ""; string sData4Encrypt = "{";
